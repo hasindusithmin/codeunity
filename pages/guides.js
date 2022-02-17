e3b0a8e78dbbc5ac61ddfde1ed6243e67d7d4d09
hasindusithmin <hasindusithmin64@gmail.com>
2022-02-17 11:03:46 +0530

    init

---
+++ pages/guides.js
@@ -0,0 +1,9 @@
+import styles from '../styles/Guides.module.css'
+
+export default function Guides() {
+  return (
+    <div className={styles.guides}>
+      <h2>All Posts</h2>
+    </div> 
+  )
+}
