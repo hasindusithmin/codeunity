6082bdd3deee10da34a6dc4a1f51452ecd2e86f8
hasindusithmin <hasindusithmin64@gmail.com>
2022-04-28 15:38:34 +0530

    edit advisor.js

--- pages/advisor.js
+++ pages/advisor.js
@@ -1,11 +1,54 @@
-
+import Link from "next/link";
+import { useContext, useEffect, useState } from "react";
+import AuthContext from "../stores/authcontext";
 export default function Advisor() {
-    function onTelegramAuth(user) {
-        console.log(user)
+    const [isSend,setIsSend] = useState(false);
+  const [notexist, setNotExist] = useState(true);
+  const [usr, setUsr] = useState(null);
+  const { user } = useContext(AuthContext);
+  const sendMessage = ()=>{
+      if (!isSend) {
+          fetch(
+            `https://api.telegram.org/${process.env.NEXT_PUBLIC_TOKEN}/sendMessage?chat_id=${usr.chat_id}&text=Hello World`
+          )
+          .then(res=>{
+              if (res.status === 200) setIsSend(true)
+          })
       }
-    return (
+  }
+  useEffect(() => {
+    if (user) {
+        fetch(
+            `https://data.mongodb-api.com/app/application-0-nfogs/endpoint/getuser?user_id=${user.id}`
+          )
+            .then((res) => res.json())
+            .then((data) => {
+              setUsr(data);
+              setNotExist(false);
+            });
+    }
+  }, [user,usr]);
+  return (
+    <>
+      {notexist && (
         <div className="w3-content w3-section">
-            <script async src="https://telegram.org/js/telegram-widget.js?19" data-telegram-login="codeunitybot" data-size="large" data-onauth="onTelegramAuth(user)" data-request-access="write"></script>
+          <Link
+            href={
+              user
+                ? `https://hasindusithmin.github.io/telegram-login/#${user.id}`
+                : "/"
+            }
+          >
+            <button className="w3-btn w3-blue" >Register</button>
+          </Link>
         </div>
-    )
+      )}
+      {!notexist && (
+          <div className="w3-content w3-section">
+              <h3>welcome {usr.first_name} {usr.last_name}</h3>
+              <button className="w3-btn w3-blue" onClick={()=>{sendMessage()}}>receive message</button>
+        </div>
+      )}
+    </>
+  );
 }
