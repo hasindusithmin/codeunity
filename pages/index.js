394f558659141dfc09539d4ff11ea27dafa50d49
hasindu sithmin <hasindusithmin64@gmail.com>
2022-05-03 14:22:15 +0000

    Forex Cross Rates Widget

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,12 @@ export default function Home() {
     const chatbot = document.createElement('script')
     chatbot.src = 'https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1'
     document.body.append(chatbot)
-    
+    const cross = document.createElement('script')
+    cross.src = 'https://s3.tradingview.com/external-embedding/embed-widget-forex-cross-rates.js'
+    cross.async = true
+    const height = navigator.userAgentData.mobile ? '300':'600'
+    cross.innerText = `{ "width": "100%", "height": ${height}, "currencies": [ "EUR", "USD", "JPY", "GBP", "CHF", "AUD", "CAD", "NZD", "SGD" ], "isTransparent": true, "colorTheme": "light", "locale": "en" }`
+    document.getElementById('cross').appendChild(cross)
   }, [])
   return (
 
@@ -15,6 +20,9 @@ export default function Home() {
         <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css" />
       </Head>
       <div className="w3-content w3-section">
+        <div className="tradingview-widget-container" id="cross">
+          <div className="tradingview-widget-container__widget"></div>
+        </div>
         <df-messenger
           intent="WELCOME"
           chat-title="codeunity support"
